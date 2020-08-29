4af84a0fa8e939392cf4e87f0846beb87eb9b908
Mansi Sharma <mansi.isha.mansi@gmail.com>
2020-08-30 01:22:13 +0530

    bug fixed health synced

--- Assets/Scripts/health.cs
+++ Assets/Scripts/health.cs
@@ -6,17 +6,40 @@ using Mirror;
 public class health : NetworkBehaviour
 {
     public const int maxHealth = 100;
-    [SyncVar(hook=nameof(OnChangeHealth))] public int currentHealth = maxHealth;
+    public int currentHealth = maxHealth;
     public RectTransform healthbar;
     public bool destroyOnDeath;
 
 
     public void TakeDamage(int amount)
     {
-        if (!GetComponent<NetworkIdentity>().hasAuthority) {
+        if (!isServer) {
+            CmdChangeHealth(amount);
             Debug.Log("I am here. This is bad :(");
             return;
         }
+        // currentHealth -= amount;
+        // if(currentHealth <= 0)
+        // {
+        //     if (destroyOnDeath) {
+        //         Destroy(gameObject);
+        //     }
+        //     currentHealth = 0;
+        //     Debug.Log("dead");
+        // }
+        RpcChangeHealth(amount);
+        // healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
+    }
+
+    // void ChangeHealth() {
+    //     // Debug.Log("Hey");
+    //     healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
+    //     // currentHealth = newHealth;
+    //     CmdChangeHealth();
+    // }
+
+    [Command]
+    void CmdChangeHealth(int amount) {
         currentHealth -= amount;
         if(currentHealth <= 0)
         {
@@ -26,33 +49,23 @@ public class health : NetworkBehaviour
             currentHealth = 0;
             Debug.Log("dead");
         }
-        ChangeHealth();
-        // healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
-    }
-
-    void OnChangeHealth(System.Int32 oldHealth, System.Int32 newHealth) {
         // Debug.Log("Hey");
         healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
         // currentHealth = newHealth;
-    }
-
-    void ChangeHealth() {
-        // Debug.Log("Hey");
-        healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
-        // currentHealth = newHealth;
-        CmdChangeHealth();
-    }
-
-    [Command]
-    void CmdChangeHealth() {
-        // Debug.Log("Hey");
-        healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
-        // currentHealth = newHealth;
-        RpcChangeHealth() ;
+        RpcChangeHealth(amount) ;
     }
 
     [ClientRpc]
-    void RpcChangeHealth() {
+    void RpcChangeHealth(int amount) {
+        currentHealth -= amount;
+        if(currentHealth <= 0)
+        {
+            if (destroyOnDeath) {
+                Destroy(gameObject);
+            }
+            currentHealth = 0;
+            Debug.Log("dead");
+        }
         // Debug.Log("Hey");
         healthbar.sizeDelta = new Vector2(currentHealth * 2, healthbar.sizeDelta.y);
         // currentHealth = newHealth;
